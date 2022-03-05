056328dd45dc9ed9e3331e32d35adda25c6d55e7
Your Name <nguyenthanhdat76199@gmail.com>
2022-03-05 22:12:11 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //Routing
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 
 //Components
 import Header from './components/Header'
